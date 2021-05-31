198ccacdb7b299ea7ea3637852b792c1b2bd7fcc
Prahalad Belavadi <github@belavadi.com>
2021-06-01 01:19:43 +0530

    generate random number

--- src/bitcoin.rs
+++ src/bitcoin.rs
@@ -1,5 +1,100 @@
-fn create_keys()->String{
-	String::from("iamaprivatekey.ipromise")
+// fn create_keys()->String{
+// 	String::from("iamaprivatekey.ipromise")
+// }
+
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_create_keys() {
+//    		let key = create_keys();
+//       println!("{}",key)
+//     }
+    
+// }
+
+
+// -----------------------
+// use rand;
+
+// pub fn create_keys()->f64{
+// 	let mut rng = rand::thread_rng();
+// 	let y: f64 = rng.gen(); 
+//   y
+// }
+
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_create_keys() {
+//    		let key = create_keys();
+//       println!("{}",key)
+//     }
+    
+// }
+
+// -------------------
+
+// use rand::Rng;
+
+// pub fn create_keys()->f64{
+// 	let mut rng = rand::thread_rng();
+// 	let y: f64 = rng.gen(); 
+//   y
+// }
+
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_create_keys() {
+//    		let key = create_keys();
+//       println!("{}",key)
+//     }
+    
+// }
+
+// -------------------
+
+
+// use rand::{thread_rng, Rng};
+
+// pub fn create_keys()->f64{
+// 	let mut rng:thread_rng = rand::thread_rng();
+// 	let y: f64 = rng.gen(); 
+//   y
+// }
+
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn test_create_keys() {
+//    		let key = create_keys();
+//       println!("{}",key)
+//     }
+    
+// }
+
+
+// ------
+
+use rand::{Rng};
+use rand::rngs::ThreadRng;
+
+pub fn create_keys()->f64{
+	let mut rng: ThreadRng= rand::thread_rng();
+	let y: f64 = rng.gen(); 
+  y
 }
 
 
